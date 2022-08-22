a2f97c76806609ab7d0e6320aad319fe7c50daa0
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-22 18:09:48 -0300

    H2 database, test profile, JPA

--- src/main/java/br/edu/ifs/course/controller/UserController.java
+++ src/main/java/br/edu/ifs/course/controller/UserController.java
@@ -1,6 +1,5 @@
 package br.edu.ifs.course.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
