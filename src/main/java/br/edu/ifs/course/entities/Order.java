5ca21bae31ee6d5754b8641e8cf904e740ede4ae
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-23 10:25:50 -0300

    OrderStatus enum

--- src/main/java/br/edu/ifs/course/entities/Order.java
+++ src/main/java/br/edu/ifs/course/entities/Order.java
@@ -14,6 +14,8 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import br.edu.ifs.course.entities.enums.OrderStatus;
+
 @Entity
 @Table(name = "tab_order")
 public class Order implements Serializable {
@@ -32,15 +34,18 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	private Integer orderStatus;
+
 	public Order() {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);;
 	}
 
 	public Long getId() {
@@ -65,6 +70,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
