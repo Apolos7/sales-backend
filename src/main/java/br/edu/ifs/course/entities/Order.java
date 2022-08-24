ffe055067f0f93919125fa6929795c6e6f989704
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-24 11:45:05 -0300

    Subtotal & Total methods

--- src/main/java/br/edu/ifs/course/entities/Order.java
+++ src/main/java/br/edu/ifs/course/entities/Order.java
@@ -104,6 +104,16 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		
+		for(OrderItem item: items) {
+			total += item.getSubTotal();
+		}
+		
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
