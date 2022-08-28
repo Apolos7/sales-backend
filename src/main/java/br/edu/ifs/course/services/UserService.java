a5d041416510f54c51de9e33ce40ff07a75ff3fe
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-28 13:03:32 -0300

    User update

--- src/main/java/br/edu/ifs/course/services/UserService.java
+++ src/main/java/br/edu/ifs/course/services/UserService.java
@@ -11,25 +11,39 @@ import br.edu.ifs.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> optional = userRepository.findById(id);
 		return optional.isPresent() ? optional.get() : null;
 	}
-	
+
 	public User insert(User user) {
 		return userRepository.save(user);
 	}
-	
+
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
 
+	public User update(Long id, User user) {
+		User entity = userRepository.getReferenceById(id);
+		System.out.println("Nome: " + entity.getName());
+		updateData(entity, user);
+		
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
+	
 }
