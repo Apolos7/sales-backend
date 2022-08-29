672de5f87eb722bf1b4040919427ed7916399c60
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-28 21:59:36 -0300

    Exception handling - delete

--- src/main/java/br/edu/ifs/course/controller/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/edu/ifs/course/controller/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.edu.ifs.course.services.exceptions.DatabaseException;
 import br.edu.ifs.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -20,6 +21,15 @@ public class ResourceExceptionHandler {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, exception.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException exception, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, exception.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
 	}
 
 }
